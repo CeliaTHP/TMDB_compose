afd937298909a69a885847095485899432bf70a5
CeliaTHP <Anohan@11>
2022-11-15 10:34:46 +0100

    - Add Hilt dependencies

--- app/src/main/java/com/example/tmdb_compose/MainActivity.kt
+++ app/src/main/java/com/example/tmdb_compose/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tmdb_compose.ui.theme.TMDB_composeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
